0ee1b5e4db130d6cff3854fa928236fb51fb5368
wisnshaftler <wisnshaftler@gmail.com>
2023-06-27 22:22:58 +0530

    four

--- webhook.mjs
+++ webhook.mjs
@@ -6,7 +6,7 @@ const __dirname = path.resolve();
 const webhook_router = express.Router();
 
 webhook_router.get("/image_callback", (req, res)=>{
-    
+    res.send({ received : true })
 })
 
 export default webhook_router;
